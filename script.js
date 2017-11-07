ae0068e03e384f53abe03bc49f3645265857897c
oliverengel <oliverengel6@gmail.com>
2017-11-07 15:58:12 -0800

    s

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function getTimeRemaining(endtime) {
+function getTimeRemaining(endtime){
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
   return {
     'total': t,
     'days': days,
@@ -13,14 +13,14 @@ function getTimeRemaining(endtime) {
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(id, endtime){
   var clock = document.getElementById(id);
   var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
 
-  function updateClock() {
+  function updateClock(){
     var t = getTimeRemaining(endtime);
 
     daysSpan.innerHTML = t.days;
@@ -28,14 +28,14 @@ function initializeClock(id, endtime) {
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-    if (t.total <= 0) {
+    if(t.total<=0){
       clearInterval(timeinterval);
     }
   }
 
   updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+  var timeinterval = setInterval(updateClock,1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = 'December 31 2015 00:00:50 UTC+0200';
 initializeClock('clockdiv', deadline);
